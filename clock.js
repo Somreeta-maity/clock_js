132e24cb82d424ef25816ced7e7ec8455ad263b4
Somreeta Maity <ateermos@gmail.com>
2021-01-04 22:34:35 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,27 @@
+var d, m, h, s,clock, color;
+function myclock(){
+    d = new Date()
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    
+    if (h <=9 ){
+        h='0' + h;
+    }
+     
+
+     
+    if (m <=9 ){
+        m = '0' + m;
+    }
+     if (s <= 9){
+         s = '0' + s;
+     }
+
+     clock = h + ":" + m + ":" + s;
+     color = "#" + h + m + s;
+     document.getElementById("clock").innerHTML = clock
+     document.body.style.background = color;
+     setTimeout(myclock,10);
+    }
+    myclock();
